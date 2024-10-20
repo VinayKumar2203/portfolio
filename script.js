c971fbe29edaeceeb75d4e92a004b6294d52f92e
Vinay Kumar <vinaysikhera2203@gmail.com>
2024-10-20 22:44:18 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,34 @@
+const menu = document.getElementById("menu");
+const remove = document.getElementById("remove");
+const navItems = document.querySelector(".navItems");
+
+menu.addEventListener("click", () => {
+    navItems.style.display = "block"
+    menu.style.display = "none";
+    remove.style.display = "block";
+});
+
+remove.addEventListener("click", () => {
+    navItems.style.display = "none"
+    menu.style.display = "block";
+    remove.style.display = "none";
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+const textArray = ['Front-end developer', 'backend developer', 'MERN Developer'];
+let currentIndex = 0;
+let homeChangeText = document.getElementById('homeChangeText');
+function changeText() {
+    console.log(textArray[currentIndex])
+    homeChangeText.textContent = textArray[changeText];
+}
+
+function updateText() {
+    currentIndex = (currentIndex + 1) % textArray.length;
+    changeText();
+}
+
+setInterval(updateText, 2000);
+changeText();
+});
